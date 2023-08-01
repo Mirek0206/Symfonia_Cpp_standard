cae2a89837b7ebda5f5fcf1e91002ead93ba271d
Damian Mirowski <damian_mirowski@wp.pl>
2023-08-01 23:03:45 +0200

    Dodano program z funkcja warunkowa "if...else".

---
+++ Rozdzial_2/Instrukcja_warunkowa_if.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    int wys, punkty_karne;
+
+    std::cout << "Na jakiej wysokosci lecimy ? [w metrach]: ";
+    std::cin >> wys;
+
+    if ( wys < 500 )
+    {
+        std::cout << "\n" << wys << " metrow to za nisko ! \n";
+        punkty_karne = 1;
+    }
+    else
+    {
+        std::cout << "\nNa wysokosci " << wys << " metrow jestes juz bezpieczny \n";
+        punkty_karne = 0;
+    }
+
+    // ------------ ocena Twoich wynikow ----------------
+    std::cout << "Masz " << punkty_karne << " punktow karnych \n";
+    if ( punkty_karne )
+    {
+        std::cout << "Popraw sie !";
+    }
+}
